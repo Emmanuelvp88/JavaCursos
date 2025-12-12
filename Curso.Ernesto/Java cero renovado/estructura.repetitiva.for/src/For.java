0369243ecf10e994a0aa5ab12bdae45714795e25
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2025-12-12 11:21:07 -0600

    Se avnaza con la leccion 12 bucles for

---
+++ Curso.Ernesto/Java cero renovado/estructura.repetitiva.for/src/For.java
@@ -0,0 +1,11 @@
+public class For {
+   public static void main(String [] args){
+       //Vamos aver como funcionan los bucles for
+
+       for(int i = 0; i <= 10; i++){
+           System.out.print(i + ",") ;
+       }/*La variable "i" se declara dentro del bucle por temas de alcannce de Scoup, esto para que
+       la variable solo sea valida dentro del este "for"*/
+
+    }
+}
