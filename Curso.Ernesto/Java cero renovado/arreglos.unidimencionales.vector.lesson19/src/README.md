888a1c43e9ac934b886fb5a39736f3065905eefa
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2025-12-22 21:35:09 -0600

    lesson 19 vectores estaticos

---
+++ Curso.Ernesto/Java cero renovado/arreglos.unidimencionales.vector.lesson19/src/README.md
@@ -0,0 +1,3 @@
+Crear un vector de cinco posiciones, posteriormente guardar un numero 
+en cada una de las posiciones del vector, y finalmente imprimir en pantalla 
+cada una de las posiciones para verificar que se hayan guardado los numerois de manera correcta.
---
+++ Curso.Ernesto/Java cero renovado/arreglos.unidimencionales.vector.lesson19/src/Vector.java
@@ -0,0 +1,20 @@
+public class Vector {
+    public static void main(String [] args){
+
+        //declaracion de un vector estatico
+        int numeros[] = new int[5];
+
+        //asignacion de parametros dentro de un vector
+        numeros[0] = 1;
+        numeros[1] = 6;
+        numeros[2] = 45;
+        numeros[3] = 567;
+        numeros[4] = 76;
+
+
+        System.out.println("La posiion numero [5] del vector contiene el numero: " + numeros[4]);
+
+
+
+    }
+}
