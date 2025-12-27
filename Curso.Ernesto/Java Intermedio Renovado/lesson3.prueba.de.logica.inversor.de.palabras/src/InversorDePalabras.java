7fc31faaf824150135a87560d491b46c4fc7f147
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2025-12-27 12:52:07 -0600

    Lesson3 ejersicio de logica, inversor de palabras

--- Curso.Ernesto/Java Intermedio Renovado/lesson3.prueba.de.logica.inversor.de.palabras/src/InversorDePalabras.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson3.prueba.de.logica.inversor.de.palabras/src/InversorDePalabras.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class InversorDePalabras {
+    public static void main(String[] args) {
+
+        String palabra = "";
+        Scanner sc = new Scanner(System.in);
+        String contador = "";
+
+        System.out.println("Ingresa una palabra para ser invertida.");
+        palabra = sc.nextLine();
+
+        //"i" es igual a cantidad de caracteres de "palabra", mientras sea mayor que decremente
+        for(int i = palabra.length();i > 0; i--){
+            contador += palabra.substring(i - 1, i); //"contador" variable acumladora va alojar caracter por caracter empezando de la ultima posicion
+        }
+        System.out.println("Palabra invertida: " + contador);
+    }
 }
