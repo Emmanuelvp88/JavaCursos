84f85c584fb63ff3c20d2716f2d5021f60d94496
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-01-20 19:38:08 -0600

    Leccion 19 cajero ATM con mi logica
    # Conflicts:
    #	Curso.Ernesto/Java Intermedio Renovado/lesson19.mi.atm/src/Deposito.java
    #	Curso.Ernesto/Java Intermedio Renovado/lesson19.mi.atm/src/Main.java
    #	Curso.Ernesto/Java Intermedio Renovado/lesson19.mi.atm/src/Operacion.java
    #	Curso.Ernesto/Java Intermedio Renovado/lesson19.mi.atm/src/Retiro.java
    #	Curso.Ernesto/Java Intermedio Renovado/out/production/lesson19.mi.atm/Cajero.class
    #	Curso.Ernesto/Java Intermedio Renovado/out/production/lesson19.mi.atm/Deposito.class
    #	Curso.Ernesto/Java Intermedio Renovado/out/production/lesson19.mi.atm/Main.class
    #	Curso.Ernesto/Java Intermedio Renovado/out/production/lesson19.mi.atm/Operacion.class
    #	Curso.Ernesto/Java Intermedio Renovado/out/production/lesson19.mi.atm/Retiro.class

--- Curso.Ernesto/Java Intermedio Renovado/lesson19.mi.atm/src/Deposito.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson19.mi.atm/src/Deposito.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
-public class Deposito extends Operacion{
+public class Deposito extends Operacion {
 
-    public Deposito(Cajero cajero){
+    public Deposito(Cajero cajero) {
         super(cajero);
     }
 
     @Override
-    public void ejecutar(){
+    public void ejecutar() {
         System.out.println("cuanto deseas depositar");
         int monto = new Scanner(System.in).nextInt();
         cajero.deposito(monto);
-        System.out.println("Tu saldo actual es: "  + cajero.getSaldo());
         System.out.println("---------------------------------------------");
         System.out.println("Tu saldo actual es: " + cajero.getSaldo());
         System.out.println("---------------------------------------------");
-    }
 
+    }
 }
