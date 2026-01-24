3705e93d23777e8935ba93044e8abf1a0467b3f7
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-01-24 12:52:35 -0600

    Subo carpeta de practicas

---
+++ Curso.Ernesto/Java cero renovado/practica/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0, b = 1, c = 0;
+
+        for (int i = 0; i < 10; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+
+            System.out.print(c + ",");
+        }
+    }
+}
