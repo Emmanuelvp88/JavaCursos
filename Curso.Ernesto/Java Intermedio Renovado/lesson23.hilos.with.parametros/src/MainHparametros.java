3289ace1a7bd0875048cf54c14e15f86ca774a0a
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-01-26 13:17:54 -0600

    lesson23 hilos con parametros

--- Curso.Ernesto/Java Intermedio Renovado/lesson23.hilos.with.parametros/src/MainHparametros.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson23.hilos.with.parametros/src/MainHparametros.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class MainHparametros {
+    static void main() {
+
+        Proceso hilo1 = new Proceso(" hilo1");
+        Proceso hilo2 = new Proceso(" hilo2");
+
+        hilo1.setParametro(3);
+        hilo2.setParametro(5);
+
+        hilo1.start();
+        hilo2.start();
+
+    }
+
 }
--- Curso.Ernesto/Java Intermedio Renovado/lesson23.hilos.with.parametros/src/Proceso.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson23.hilos.with.parametros/src/Proceso.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Proceso extends Thread {
+    int parametro;
 
-public class Proceso {
+    public Proceso(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i < parametro; i++){
+            System.out.println(i + getName());
+        }
+        System.out.println("");
+    }
+
+    public void setParametro(int parametro){
+        this.parametro = parametro;
+    }
+
 }
