86c16ff93c06d2c8d3e70c1abbeee1f6f2812617
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-01-28 11:30:01 -0600

    Agregando nuevos comentarios en la clase main leccion 23

--- Curso.Ernesto/Java Intermedio Renovado/lesson23.hilos.with.parametros/src/MainHparametros.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson23.hilos.with.parametros/src/MainHparametros.java
@@ -3,13 +3,14 @@ public class MainHparametros {
 
         /*al crear estos objetos mandamos el parametro de tipo "String" el
         cual lo debe de recibirlo un constructor en la clase padre */
-
         Proceso hilo1 = new Proceso(" hilo1");
         Proceso hilo2 = new Proceso(" hilo2");
 
+        //envio de parametros mediante los objetos
         hilo1.setParametro(3);
         hilo2.setParametro(5);
 
+        //Metodo Start() para arrancar los hilos
         hilo1.start();
         hilo2.start();
 
