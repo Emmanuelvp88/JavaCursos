8bc8dfdf43730c84170a595a00464d6baeb16233
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-01-29 13:31:53 -0600

    Subiendo la leccion 24 estados de un hilo

---
+++ Curso.Ernesto/Java Intermedio Renovado/lesson24.estads.de.un.hilo/src/EstadoMain.java
@@ -0,0 +1,29 @@
+public class EstadoMain {
+    public static void main(String[] args) {
+
+        //Primer estado de un hilo es la creacion del hilo
+        Proceso hilo1 = new Proceso();
+        Proceso hilo2 = new Proceso();
+
+
+        //Segundo estdo de un hilo e el arranque del hilo
+        hilo1.start();
+
+        //el tercer estado de un hilo es cuando lo bloqueamos por un lapso determida, le damos un "Delay"
+        try {
+            hilo1.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("Hilo interrumpido" + e.getMessage());
+        }
+
+
+        hilo2.start();
+        //con el metodo "sleep()" le damos un Delay al hilo en milisegundos, este metodo
+        //es forzoso que este dentro de un "try, catch"
+        try {
+            hilo2.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println("Hilo finalizado" + e.getMessage());
+        }
+    }
+}
