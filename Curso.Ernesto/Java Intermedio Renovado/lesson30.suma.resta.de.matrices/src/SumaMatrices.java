8d147daf95d5b079cfa2bece831bbfa788480602
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-02-11 11:34:36 -0600

    Subiendo la leccion 30 suma de matrices

--- Curso.Ernesto/Java Intermedio Renovado/lesson30.suma.resta.de.matrices/src/SumaMatrices.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson30.suma.resta.de.matrices/src/SumaMatrices.java
@@ -6,36 +6,49 @@ public class SumaMatrices {
 
         //for para rellenar "matriz1"
         for(int i = 0; i < matriz1.length; i++) {
-            for(int j = 0; j < matriz1[i].length; j++) {
-                matriz1[i][j] = (int)(Math.random()*100);
+            for(int j = 0; j < matriz1.length; j++) {
+                matriz1[i][j] = (int)(Math.random()*10);
             }
         }
 
         //for para rellenar matriz2
         for(int i = 0; i < matriz2.length; i ++){
-            for(int j = 0; j < matriz2[i].length; j++){
-                matriz2[i][j] = (int)(Math.random()*100);
+            for(int j = 0; j < matriz2.length; j++){
+                matriz2[i][j] = (int)(Math.random()*10);
             }
         }
 
         //for para sumar matriz1 y matriz2
         for(int i = 0; i < matriz1.length; i++){
-            for(int j = 0; j < matriz1[i].length; j++){
+            for(int j = 0; j < matriz1.length; j++){
                 resultado[i][j] = matriz1[i][j]  + matriz2[i][i];
             }
         }
 
         //For para imprimir los resultados
         for(int i = 0; i < matriz1.length; i++) {
-            for(int j = 0; j < matriz1[i].length; j++) {
-                System.out.print(" M1(" + matriz1 [i][j] + ")");
+            for(int j = 0; j < matriz1.length; j++) {
+                System.out.print("m1(" + matriz1 [i][j] + ")");
+            }
+            if (i == 1){
+                System.out.print(" + ");
+            }else {
+                System.out.print("   ");
             }
             for(int j = 0; j < matriz1[i].length; j++) {
-                System.out.print(" M2(" + matriz2 [i][j] + ")");
+                System.out.print("m2(" + matriz2 [i][j] + ")");
             }
-            System.out.println(" ");
+            if (i == 1){
+                System.out.print(" = ");
+            }else{
+                System.out.print("    ");
+            }
+            for(int j = 0; j < matriz1[i].length; j++){
+                System.out.print("mr(" + resultado[i][j] + ")");
+            }
+            System.out.println();
         }
-        System.out.println(" + ");
+
 
 //        for(int i = 0; i < matriz2.length; i++) {
 //            for(int j = 0; j < matriz2[i].length; j++) {
