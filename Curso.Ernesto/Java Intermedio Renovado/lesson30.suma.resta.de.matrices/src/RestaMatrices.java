36eed64d1bac3b8681e996a82a2c09406c71c556
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-02-16 13:30:37 -0600

    Se rellena la clase resta de matrices

--- Curso.Ernesto/Java Intermedio Renovado/lesson30.suma.resta.de.matrices/src/RestaMatrices.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson30.suma.resta.de.matrices/src/RestaMatrices.java
@@ -1,2 +1,63 @@
 public class RestaMatrices {
+    public static void main(String[] args) {
+        //Declaracion de matrices
+        int matriz1[][] = new int[3][3];
+        int matriz2[][] = new int[3][3];
+        int resultado[][] = new int[3][3];
+
+        //for para rellenar "matriz1"
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1.length; j++) {
+                matriz1[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        //for para rellenar matriz2
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2.length; j++) {
+                matriz2[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        //for para sumar matriz1 y matriz2
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+                resultado[i][j] = matriz1[i][j] - matriz2[i][j];
+            }
+        }
+
+        //For para imprimir los resultados
+        for (int i = 0; i < matriz1.length; i++) {
+
+            //recorre la 'matriz 1'
+            for (int j = 0; j < matriz1[i].length; j++) {
+                System.out.print("m1[" + matriz1[i][j] + "]");
+            }
+
+            //if para darle la identacion entre matrices "+"
+            if (i == 1) {
+                System.out.print(" + ");
+            } else {
+                System.out.print("   ");
+            }
+
+            //recorre las filas de la 'matriz 2'
+            for (int j = 0; j < matriz1[i].length; j++) {
+                System.out.print("m2[" + matriz2[i][j] + "]");
+            }
+
+            //if para darla idetacion entre matrices "="
+            if (i == 1) {
+                System.out.print(" =  ");
+            } else {
+                System.out.print("    ");
+            }
+
+            //recorre las filas de la matriz 'resultados'
+            for (int j = 0; j < matriz1[i].length; j++) {
+                System.out.print("mr[" + resultado[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
 }
