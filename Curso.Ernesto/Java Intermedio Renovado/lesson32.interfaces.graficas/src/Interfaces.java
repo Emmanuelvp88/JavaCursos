6b7674b9d9842fe76da6f079ff89bfd0505f6eb6
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-02-21 14:45:42 -0600

    Subiendo leccion 32 interfaces con IDE

---
+++ Curso.Ernesto/Java Intermedio Renovado/lesson32.interfaces.graficas/src/Interfaces.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Interfaces extends JFrame implements ActionListener{
+    private JTextField field1;
+    private JLabel label1;
+    private JButton boton1;
+
+
+    public Interfaces(){
+
+
+    }
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
