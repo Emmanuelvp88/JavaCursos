150c77882d1118af51eedf8eb812aad9f0ea3958
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-02-25 00:21:44 -0600

    Subiendo la midificacion de leccion 22 y 23

--- Curso.Ernesto/Java cero renovado/lesson22Y23.arreglos.bidimencionales.matrices.dinamicas/src/MatricesDinamicas.java
+++ Curso.Ernesto/Java cero renovado/lesson22Y23.arreglos.bidimencionales.matrices.dinamicas/src/MatricesDinamicas.java
@@ -10,26 +10,22 @@ public class MatricesDinamicas {
         System.out.println("cuantas columnas deseas?");
         int columnas = sc.nextInt();
 
-        //Declaracion del array
-        int matriz[][] = new int[filas][columnas];
+        //Declaración del array
+        int[][] matriz = new int[filas][columnas];
 
         //bucle para rellenar el for
         for (int i = 0; i < matriz.length; i++) {//recorre filas
-
             for (int j = 0; j < matriz[i].length; j++) {//recorre columnas, matriz[i] le dice que recorra los elementos de la fila "i"
-                System.out.println("dame el valos de la posicion fila " + (i + 1) + " columna " + (j + 1));
-                matriz[i][j] = sc.nextInt();//para que el usuario ingrese manualmente los numeros que desea que acontenga el array
+                System.out.println("dame el valor de la posición fila " + (i + 1) + " columna " + (j + 1));
+                matriz[i][j] = sc.nextInt();//para que el usuario ingrese manualmente los números que desea que contenga el array
             }
         }
         //for para mandar a imprimir el contenido del array
         for (int i = 0; i < matriz.length; i++) {//recorre filas
-
-            for (int j = 0; j < matriz[i].length; j++){//resorre columnas
-                System.out.print("[" + matriz[i][j]+"]");
+            for (int j = 0; j < matriz[i].length; j++) {//recorre columnas
+                System.out.print("[" + matriz[i][j] + "]");
             }
             System.out.println();
         }
-
     }
-
 }
