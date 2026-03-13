e2e1281298f89844f99e5966d1b6dc09dad592ce
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-03-13 12:01:52 -0600

    Se termino de hacer los metodos en la clase pila leccion34

--- Curso.Ernesto/Java Intermedio Renovado/lesson34.analizador.sintactico/src/Pila.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson34.analizador.sintactico/src/Pila.java
@@ -6,19 +6,34 @@ public class Pila {
     }
 
     // Metodo para insertar en la pila
-    public void insertar(char valor){
+    public void insertar(char valor) {
         Nodo nuevo = new Nodo();
         nuevo.dato = valor;
 
-        if(top == null){
-            top = null;
+        if (top == null) {
+            nuevo.siguiente = null;
             top = nuevo;
 
-        }else {
+        } else {
             nuevo.siguiente = top;
             top = nuevo;
         }
+    }
 
+    //Metodo para extraer de la pila
+    public char extraer() {
+        if (top != null) {
+            char auxiliar = top.dato;
+            top = top.siguiente;
+            return auxiliar;
+        } else {
+            return '\0';
+        }
+    }
+
+    //Metodo para saber si la pila esta vacia
+    public boolean pilaVacia() {
+        return top == null;
     }
 
 }
