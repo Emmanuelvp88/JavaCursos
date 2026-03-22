d50759b5759640adf106bf3b58163a118c6e2371
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-03-22 13:35:00 -0600

    Agregando el metodo main y el menu de opciones

--- Curso.Ernesto/Java Intermedio Renovado/lesson35.listas.tipo.cola/src/Main.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson35.listas.tipo.cola/src/Main.java
@@ -1,4 +1,44 @@
+import javax.swing.*;
+
 public class Main {
 
+    public static void Main(String[] args) {
+        int opcion = 0;
+        int informacion = 0;
+        Cola cola = new Cola();
 
+        do {
+
+            try {
+
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                        "Menú de Opciones\n\n"
+                                + "1. Insertar un nodo\n"
+                                + "2. Extraer un nodo\n"
+                                + "3. Mostrar contenido de la Cola\n"
+                                + "4. Salir.\n\n"));
+
+
+                switch (opcion) {
+                    case 1:
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+
+                }
+
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+
+        } while (opcion != 4);
+    }
 }
