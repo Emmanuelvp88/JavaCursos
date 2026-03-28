f5a8de2c43373789e713372a0a82807c5c472d88
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-03-28 11:42:15 -0600

    Correccion de comentarion en leccion 36

--- Curso.Ernesto/Java Intermedio Renovado/lesson36.recursividad/src/Recursivo.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson36.recursividad/src/Recursivo.java
@@ -2,13 +2,13 @@ public class Recursivo {
 
     public void imprimir(int valor) {
         if (valor <= 5) {
-            //imprime al subir si el print esta antes del llamado recursivo imorime de menor a mayor
+            //imprime al subir si el print esta antes del llamado recursivo imprime de menor a mayor
             System.out.print(" " + valor);
 
-            //llmado recursivo del mismo metodo con el argumento de "+1" para que vaya aumentado
+            //llamado recursivo del mismo método con el argumento de "+1" para que vaya aumentando
             imprimir(valor + 1);
 
-            //Curiosamente cuando ponemso el print despues de la llmada recursiva, se imprime de mayor a menor
+            //Curiosamente, cuando ponemos el print después de la llamada recursiva, se imprime de mayor a menor
             System.out.print(" " + valor);
         }
     }
