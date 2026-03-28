672fc87458aba8eba38ad3ea44e0c370abf5b9b8
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-03-28 14:01:59 -0600

    Termino de leccion 37

--- Curso.Ernesto/Java Intermedio Renovado/lesson37.recursividad.calcular.factorial/src/Main.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson37.recursividad.calcular.factorial/src/Main.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Cual es el numero que deseas calcular el factorial");
         int parametro = sc.nextInt();
 
         Recursividad recursividad = new Recursividad();
         int resulFactorial = recursividad.factorial(parametro);
+
         System.out.println("El factorial de " + parametro + " es: " + resulFactorial);
+        sc.close();
     }
 }
