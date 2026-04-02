7dd8527a1f3e3ea13878251d7456105fa17f1438
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-04-02 14:19:41 -0600

    Se creo el frame para la actividad

--- Curso.Ernesto/Java Intermedio Renovado/lesson38.class.graphics/src/Formas.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson38.class.graphics/src/Formas.java
@@ -1,2 +1,25 @@
-public class Formas {
+import javax.swing.*;
+
+public class Formas extends JFrame {
+
+    public Formas(){
+        initComponents();
+        this.setBounds(0,0,600,400);
+        this.setLocationRelativeTo(null);
+    }
+
+
+    public static void main(String [] args){
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Formas().setVisible(true);
+            }
+        });
+
+    }
+
+
+
+
 }
