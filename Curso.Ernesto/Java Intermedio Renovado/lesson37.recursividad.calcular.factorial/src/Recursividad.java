3f7376eb37ef2bf8402dd4dc425339997f1cece4
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-04-02 13:20:00 -0600

    terminando la leccion 37

--- Curso.Ernesto/Java Intermedio Renovado/lesson37.recursividad.calcular.factorial/src/Recursividad.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson37.recursividad.calcular.factorial/src/Recursividad.java
@@ -1,7 +1,10 @@
 public class Recursividad {
     public int factorial(int parametro){
+
         if(parametro > 0){
+
             int auxiliar = parametro * factorial(parametro - 1);
+            System.out.println(auxiliar);
             return auxiliar;
         }
 
