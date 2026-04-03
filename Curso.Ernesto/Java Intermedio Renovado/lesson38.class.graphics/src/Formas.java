f81e677e03a6971672cca89e33fb9a0718607b8c
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-04-03 13:04:37 -0600

    Se agregan comentarios de referencia

--- Curso.Ernesto/Java Intermedio Renovado/lesson38.class.graphics/src/Formas.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson38.class.graphics/src/Formas.java
@@ -23,15 +23,18 @@ public class Formas extends JFrame {
     public void paint(Graphics g){
         super.paint(g);
 
-        g.setColor(Color.MAGENTA);
-        g.drawLine(10,100,100,100);
-        g.draw3DRect(150,50,60,100,true);
-        g.drawRoundRect(250, 50, 60, 100, 6, 6);
-        g.drawOval(350, 50, 60,100);
 
+        g.setColor(Color.MAGENTA); //Para establecer un color
+        g.drawLine(10,100,100,100); //Para dibujar una liena
+        g.drawRect(150,50,60,100); //Para dibijar un rectangulo
+        g.drawRoundRect(250, 50, 60, 100, 6, 6); //Para dibujar un rectangulo con bordes redondeados
+        g.drawOval(350, 50, 60,100); //Paa dibujar un Ovalo
+
+        //vectores para darle cordenadas a un trinagulo
+        //De atras hacia deante seria vertice A,B y C
         int vectorX [] = {500, 550, 450};
         int vectorY [] = {50, 140, 140};
-        g.drawPolygon(vectorX, vectorY, vectorX.length);
+        g.drawPolygon(vectorX, vectorY, vectorX.length); //Para dibujar un triangulo con los vectores acteriores
 
 
 
