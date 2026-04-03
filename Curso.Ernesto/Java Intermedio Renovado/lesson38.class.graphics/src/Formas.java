29a066bdfa5681109310be03b36e8d5e8d198346
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-04-03 12:26:59 -0600

    Se terminan de dibijar las formas geometricas

--- Curso.Ernesto/Java Intermedio Renovado/lesson38.class.graphics/src/Formas.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson38.class.graphics/src/Formas.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Formas extends JFrame {
 
     public Formas(){
-        initComponents();
+//        initComponents();
         this.setBounds(0,0,600,400);
         this.setLocationRelativeTo(null);
     }
@@ -19,7 +20,21 @@ public class Formas extends JFrame {
 
     }
 
+    public void paint(Graphics g){
+        super.paint(g);
+
+        g.setColor(Color.MAGENTA);
+        g.drawLine(10,100,100,100);
+        g.draw3DRect(150,50,60,100,true);
+        g.drawRoundRect(250, 50, 60, 100, 6, 6);
+        g.drawOval(350, 50, 60,100);
+
+        int vectorX [] = {500, 550, 450};
+        int vectorY [] = {50, 140, 140};
+        g.drawPolygon(vectorX, vectorY, vectorX.length);
 
 
+
+    }
 
 }
