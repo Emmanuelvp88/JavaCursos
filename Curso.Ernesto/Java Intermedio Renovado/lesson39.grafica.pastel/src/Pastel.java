8eac26a689a70b914cd229c707af71ff88f3b58b
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-04-04 16:06:24 -0600

    Se avanza con el programa

--- Curso.Ernesto/Java Intermedio Renovado/lesson39.grafica.pastel/src/Pastel.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson39.grafica.pastel/src/Pastel.java
@@ -2,5 +2,20 @@ import javax.swing.*;
 
 public class Pastel extends JFrame {
 
+    public Pastel(){
+        this.setBounds(0,0,400,390);
+        this.setLocationRelativeTo(null);
+        this.setTitle("Pastel");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    Pastel pastel = new Pastel();
+
+    boolean bandera = false;
+
+    public static void main(String [] args){
+
+    }
+ private boton1 = pastel.
 
 }
