463b17ca18ff15db4c84ca2cb0bd55f698967229
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-04-06 13:33:25 -0600

    Se agrega el nujeo panel

--- Curso.Ernesto/Java Intermedio Renovado/lesson39.grafica.pastel/src/Pastel.java
+++ Curso.Ernesto/Java Intermedio Renovado/lesson39.grafica.pastel/src/Pastel.java
@@ -7,21 +7,38 @@ public class Pastel extends JFrame {
     JTextField txtRojo, txtVerde, txtAzul;
     JButton graficar;
     JPanel panelGrafica;
+    int rojo, verde, azul;
 
     public Pastel(){
-        this.setBounds(0,0,400,390);
+        this.setTitle("Grafica de Pastel");
+        this.setSize(500, 450);
         this.setLocationRelativeTo(null);
-        this.setTitle("Pastel");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        //mandamos a llamar al metodo que contiene lso componentes
+        initComponent();
     }
 
+   panelGrafica = new JPanel(){
+        @Override
+                protected void paintComponent(Graphics g);
+    };
 
     boolean bandera = false;
 
-    public static void main(String [] args){
+
+
+
+
+    public void initComponent(){
 
     }
-// private boton1 = pastel.
+
+
+
+    public static void main(String [] args){
+    }
+
 
 }
